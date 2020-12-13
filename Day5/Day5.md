58e2d6b27cb1872d20d9281d3b914a40489899b9
IciaCarroBarallobre <iciacarrobarallobre@gmail.com>
2020-12-13 22:06:59 +0100

    Day5

--- Day5/Day5.md
+++ Day5/Day5.md
@@ -22,7 +22,7 @@ For example, consider just the first seven characters of `FBFBBFFRLR`:
 - F keeps rows 44 through 45.
 - The final F keeps the lower of the two, row 44.
 
-The last three characters will be either L or R; these specify exactly one of the 8 columns of seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time with only three steps. L means to keep the lower half, while R means to keep the upper half.
+The **last three characters** will be either L or R; these specify exactly one of the **8 columns** of seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time with only three steps. L means to keep the lower half, while R means to keep the upper half.
 
 For example, consider just the last 3 characters of FBFBBFFRLR:
 
@@ -45,4 +45,14 @@ Here are some other boarding passes:
 
 As a sanity check, look through your list of boarding passes. 
 
-**What is the highest seat ID on a boarding pass?**
+**What is the highest seat ID on a boarding pass?** `980`
+
+#### Part 2
+
+*Ding!* The "fasten seat belt" signs have turned on. Time to find your seat.
+
+It's a completely full flight, so your seat should be the only missing boarding pass in your list. However, there's a catch: some of the seats at the very front and back of the plane don't exist on this aircraft, so they'll be missing from your list as well.
+
+Your seat wasn't at the very front or back, though; the seats with **IDs +1** and **-1** from yours will be in your list.
+
+**What is the ID of your seat?** `607`
--- Day5/day5.erl
+++ Day5/day5.erl
@@ -1,57 +1,60 @@
 -module(day5).
 -import(math,[pow/2]).
--export([read/1,solve/1]).
+-export([read/1,toIDs/1, ourNumber/1]).
 
-%%%%%%% READ AND PARSE THE INPUT 
+%%%%%%%%% READ AND PARSE THE INPUT 
 
-map(F, List) ->
-    [F(X) || X<-List].
+map(F, List) -> [F(X) || X<-List].
 
 read(FileName) ->
   {ok, Binary} = file:read_file(FileName),
   string:tokens(erlang:binary_to_list(Binary), "\n").
 
 %%%%%%%%% AUX
-tobinarytuples(String, Acc) -> 
-case String of
-    [H|T]->
-            if 
-                (([H] == "B") or ([H] == "R")) -> NewAcc = Acc ++ "1";
-                true -> NewAcc = Acc ++ "0"
-            end,
-            tobinarytuples(T,NewAcc);
-    [] -> {
-            string:substr(Acc,1,7),
-            string:substr(Acc,8,3)
-        }
-end.
 
 tobinarytuples(String) -> tobinarytuples(String,"").
+
+tobinarytuples([], Acc) -> {string:substr(Acc,1,7),string:substr(Acc,8,3)};
+tobinarytuples([H|T], Acc) -> 
+    if 
+        (([H] == "B") or ([H] == "R")) -> NewAcc = Acc ++ "1";
+        true -> NewAcc = Acc ++ "0"
+    end,
+    tobinarytuples(T,NewAcc).
+
 
 binarytodecimal(Number) -> 
     Resverse = string:reverse(Number), 
     binarytodecimal(Resverse,0, 0).
 
-binarytodecimal(Number, Index, Aux) ->
-    case  Number of
-        [H|T]-> if 
-                    ([H] == "1") -> 
-                        binarytodecimal(T, Index+1, Aux+pow(2,Index));
-                    true -> 
-                        binarytodecimal(T, Index+1, Aux)
-                end;
-        []-> Aux
+binarytodecimal([], _, Aux)-> Aux;
+binarytodecimal([H|T], Index, Aux) ->
+    if 
+        ([H] == "1") -> 
+            binarytodecimal(T, Index+1, Aux+pow(2,Index));
+        true -> 
+            binarytodecimal(T, Index+1, Aux)
     end.
 
 binaryToDecimalTuple({N1,N2}) -> {binarytodecimal(N1), binarytodecimal(N2)}.
 
+%%%%%%%%% SOLVE 
+
 calculateID({Row,Column}) -> Row *8 + Column.
 
-solve(List) -> map(fun(String)->calculateID(binaryToDecimalTuple(tobinarytuples(String))) end, List).
+toIDs(List) -> map(fun(String)->calculateID(binaryToDecimalTuple(tobinarytuples(String))) end, List).
 
+ourNumber([H1,H2|T]) ->
+    if  
+        (H1 == H2 - 2 ) -> H1+1; 
+        true -> ourNumber([H2|T])
+    end;
+ourNumber(_) -> notfound.   
 
-%%%%%%%%%%%%%% SOLVE 
+%%%%%%%%% Results:
+%%
 %% c(day5).
 %% L  =  day5:read("input.txt").
-%% IDList = day5:solve(L).
-%% lists:max(IDList)
+%% IDList = day5:toIDs(L).
+%% lists:max(IDList).
+%% day5:ourNumber(lists:sort(IDList)).
